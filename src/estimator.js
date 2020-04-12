cbe6dec40a2cceee6df98d4e1b76d5c49eddca7c
temitope-israel <omoniyitemitopeisrael@gmail.com>
2020-04-12 23:16:26 +0100

    D

--- src/estimator.js
+++ src/estimator.js
@@ -1,12 +1,9 @@
 const covid19ImpactEstimator = (data) => {
-  
   data; 
   const impact = {};
   const severeImpact = {};
 
   // CHALLENGE ONE -// ESTIMATING THE NUMBER OF CURRENTLY INFECTED PEOPLE
-  let currentlyInfectedImpact;
-  let currentlyInfectedSevereImpact;
   const reportedCases = data.reportedCases;
   
   impact.currentlyInfected = reportedCases * 10;
