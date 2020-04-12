437ae5df1c05b77d590e30d80fa68e83a2bac1b3
temitope-israel <omoniyitemitopeisrael@gmail.com>
2020-04-13 00:36:51 +0100

    E2

--- src/estimator.js
+++ src/estimator.js
@@ -1,17 +1,17 @@
 const covid19ImpactEstimator = (data) => {
-  inputData = data;
-  inputData ={}
-  const impact ={};
-  const severeImpact ={};
+  data;
+  inputData = {}
+  const impact = {};
+  const severeImpact = {};
 
   inputData.reportedCases = '';
-  impact.currentlyInfected = inputData.reportedCases * 10;
-  severeImpact.currentlyInfected = input.reportedCases * 50;
+  impact.currentlyInfected = data.reportedCases * 10;
+  severeImpact.currentlyInfected = data.reportedCases * 50;
 
   impact.infectionsByRequestedTime = impact.currentlyInfected * 512;
   severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 512;
 
-  return{impact, severeImpact};
-  }
+  return {impact, severeImpact};
+};
 
-  export default covid19ImpactEstimator;
+export default covid19ImpactEstimator;
