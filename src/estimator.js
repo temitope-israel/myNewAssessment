bcd066abb9a12ad6386b7226e2fbf356b774ee5a
temitope-israel <omoniyitemitopeisrael@gmail.com>
2020-04-12 22:13:13 +0100

    more changes

--- src/estimator.js
+++ src/estimator.js
@@ -2,34 +2,31 @@ const covid19ImpactEstimator = (data) => {
   // INPUT DATA
   
   // OUTPUT DATA
-      impact= {
+  let impact = {
       currentlyInfected: '',
       infectionsByRequestedTime: ''
    };
 
-      severeImpact= {
-      currentlyInfected: '',
-      infectionsByRequestedTime: ''
-   };
+   let severeImpact = {
+    currentlyInfected: '',
+    infectionsByRequestedTime: ''
+  };
+
+  // CHALLENGE ONE
+  let currentlyInfected = reportedCases * 10;
+  myOutput.impact.currentlyInfected = currentlyInfected;
 
 
+  let severeCurrentlyInfected = data.reportedCases * 50;
+  myOutput.severeImpact.currentlyInfected = severeCurrentlyInfected;
 
+  let infectionsByRequestedTime = myOutput.impact.currentlyInfected * 512;
+  myOutput.impact.infectionsByRequestedTime = infectionsByRequestedTime;
 
-// CHALLENGE ONE
-let currentlyInfected = reportedCases * 10;
-myOutput.impact.currentlyInfected = currentlyInfected;
+  let severeInfectionsByRequestedTime = myOutput.severeImpact.currentlyInfected * 512;
+  myOutput.severeImpact.infectionsByRequestedTime = severeInfectionsByRequestedTime;
 
-
-let severeCurrentlyInfected = data.reportedCases * 50;
-myOutput.severeImpact.currentlyInfected = severeCurrentlyInfected;
-
-let infectionsByRequestedTime = myOutput.impact.currentlyInfected * 512;
-myOutput.impact.infectionsByRequestedTime = infectionsByRequestedTime;
-
-let severeInfectionsByRequestedTime = myOutput.severeImpact.currentlyInfected * 512;
-myOutput.severeImpact.infectionsByRequestedTime = severeInfectionsByRequestedTime;
-
-}
+  }
 
 export default covid19ImpactEstimator;
 
