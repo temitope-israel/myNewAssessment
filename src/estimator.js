3b9a5ffd45459e483828faa1cf00ec6c28b7b7db
temitope-israel <omoniyitemitopeisrael@gmail.com>
2020-04-12 21:24:11 +0100

    Challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,51 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = data => {
+let impact;
+let severeImpact;
+// INPUT DATA
+data = {
+   reportedCases: ''
+}
+  
+// OUTPUT DATA
+myOutput = {
+   data: {},
+   impact: {
+      currentlyInfected: '',
+      infectionsByRequestedTime: ''
+   },
+   severeImpact: {
+      currentlyInfected: '',
+      infectionsByRequestedTime: ''
+   }
+}
 
-export default covid19ImpactEstimator;
+
+
+// CHALLENGE ONE
+let currentlyInfected = reportedCases * 10;
+myOutput.impact.currentlyInfected = currentlyInfected;
+
+
+let severeCurrentlyInfected = data.reportedCases * 50;
+myOutput.severeImpact.currentlyInfected = severeCurrentlyInfected;
+
+let infectionsByRequestedTime = myOutput.impact.currentlyInfected * 512;
+myOutput.impact.infectionsByRequestedTime = infectionsByRequestedTime;
+
+let severeInfectionsByRequestedTime = myOutput.severeImpact.currentlyInfected * 512;
+myOutput.severeImpact.infectionsByRequestedTime = severeInfectionsByRequestedTime;
+
+
+
+
+
+
+
+
+
+
+}
+
+//export default covid19ImpactEstimator;
+
+
