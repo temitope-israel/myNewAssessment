35b5eb19dbd666a21a4d17161b26c0447600bad8
temitope-israel <omoniyitemitopeisrael@gmail.com>
2020-04-14 17:34:33 +0100

    A6

--- src/estimator.js
+++ src/estimator.js
@@ -7,35 +7,43 @@ const covid19ImpactEstimator = (data) => {
   impact.currentlyInfected = currentlyInfected;
   const severeCurrentlyInfected = input.reportedCases * 50;
   severeImpact.currentlyInfected = severeCurrentlyInfected;
-
-  const infectionsByRequestedTime = impact.currentlyInfected * 512;
-  impact.infectionsByRequestedTime = infectionsByRequestedTime;
-
-  const severeInfectionsByRequestedTime = severeImpact.currentlyInfected * 512;
-  severeImpact.infectionsByRequestedTime = severeInfectionsByRequestedTime;
-
+  
   function normaliseDays(){
-    let days = 1024 ;
-    
-    
+    let days = 1024;
+    let weeks = 146;
+    let months = 34;
+  
     if(input.periodType === days){
-        days = currentlyInfected * days;
+        days = impact.currentlyInfected * days;
         impact.infectionsByRequestedTime = days;
-        //severeImpact.infectionsByRequestedTime = days;
-    
-    };
+
+        days = severeImpact.currentlyInfected * days;
+        severeImpact.infectionsByRequestedTime = days;    
+    } else if(input.periodType === weeks){
+        weeks = impact.currentlyInfected * weeks;
+        impact.infectionsByRequestedTime = weeks;
+
+        days = severeImpact.currentlyInfected * weeks;
+        severeImpact.infectionsByRequestedTime = weeks;
+    } else //(input.periodType === months){
+        months = impact.currentlyInfected * months;
+        impact.infectionsByRequestedTime = months;
+
+        months = severeImpact.currentlyInfected * months;
+        severeImpact.infectionsByRequestedTime = months;      
     }
 
   return {
     impact: {
       currentlyInfected,
-      infectionsByRequestedTime
+      infectionsByRequestedTime = normaliseDays()
     },
 
     severeImpact: {
       currentlyInfected,
-      infectionsByRequestedTime
+      infectionsByRequestedTime = normaliseDays()
     }
   };
-};
+}
+
 export default covid19ImpactEstimator;
