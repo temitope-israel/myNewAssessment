d2c444caa451564d914304cfb96d72418d23e4b0
temitope-israel <omoniyitemitopeisrael@gmail.com>
2020-04-14 13:51:33 +0100

    A2

--- src/estimator.js
+++ src/estimator.js
@@ -1,32 +1,26 @@
 const covid19ImpactEstimator = (data) => {
-    data;
+  data;
 
-
-let currentlyInfected, 
-severeCurrentlyInfected,
-infectionsByRequestedTime,
-severeInfectionsByRequestedTime;
-
-currentlyInfected = reportedCases(data) * 10;
+const currentlyInfected = reportedCases(data) * 10;
 impact.currentlyInfected = currentlyInfected;
-severeCurrentlyInfected = reportedCases(data) * 50;
+const severeCurrentlyInfected = reportedCases(data) * 50;
 severeImpact.currentlyInfected = severeCurrentlyInfected;
 
-infectionsByRequestedTime = impact.currentlyInfected * 512;
+const infectionsByRequestedTime = impact.currentlyInfected * 512;
 impact.infectionsByRequestedTime = infectionsByRequestedTime;
 
-severeInfectionsByRequestedTime = severe.currentlyInfected * 512;
-severe.infectionsByRequestedTime = severeInfectionsByRequestedTime;
-return{
+const severeInfectionsByRequestedTime = severeImpact.currentlyInfected * 512;
+severeImpact.infectionsByRequestedTime = severeInfectionsByRequestedTime;
+  return {
     impact: {
-        currentlyInfected,
-        infectionsByRequestedTime
+      currentlyInfected,
+      infectionsByRequestedTime
     },
 
     severeImpact: {
-        currentlyInfected,
-        infectionsByRequestedTime
+      currentlyInfected,
+      infectionsByRequestedTime
     }
-}
-}
+};
+};
 export default covid19ImpactEstimator;
