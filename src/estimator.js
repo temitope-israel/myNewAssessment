725c2cdc5f0635340f25d100ca5a071e4ad5d5a9
temitope-israel <omoniyitemitopeisrael@gmail.com>
2020-04-15 11:19:51 +0100

    B55

--- src/estimator.js
+++ src/estimator.js
@@ -1,4 +1,5 @@
 const covid19ImpactEstimator = (data) =>{
+  let reportedCases;
   data.reportedCases = reportedCases;
   reportedCases = data.currentlyInfected * 10;
   impact.currentlyInfected = reportedCases;
