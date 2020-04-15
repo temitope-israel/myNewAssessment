6ab2cb723af44d90e58f66cd4f17a2d45e80072d
temitope-israel <omoniyitemitopeisrael@gmail.com>
2020-04-15 11:02:07 +0100

    B4

--- src/estimator.js
+++ src/estimator.js
@@ -1,39 +1,9 @@
 const covid19ImpactEstimator = (data) => {
   const input = data;
-  let impact;
-  let severeImpact;
-  let infectionsByRequestedTime;
-  let periodType;
-
-  const currentlyInfected = input(data).reportedCases * 10;
-  impact.currentlyInfected = currentlyInfected;
-  const severeCurrentlyInfected = input(data).reportedCases * 50;
-  severeImpact.currentlyInfected = severeCurrentlyInfected;
-  input.periodType = periodType;
-  const days = 1024;
-  let weeks;
-  let months;
-  if (periodType === weeks) {
-    periodType = days / 7;
-  } else if (periodType === months) {
-    periodType = days / 30;
-  } else periodType = days;
-
-  infectionsByRequestedTime = impact.currentlyInfected * periodType;
-  impact.infectionsByRequestedTime = infectionsByRequestedTime;
-  infectionsByRequestedTime = severeImpact.currentlyInfected * periodType;
-  severeImpact.infectionsByRequestedTime = infectionsByRequestedTime;
-
+  
   return {
-    impact: {
-      currentlyInfected,
-      infectionsByRequestedTime
-    },
-
-    severeImpact: {
-      currentlyInfected,
-      infectionsByRequestedTime
-    }
+    impact: {},
+    severeImpact: {}
   };
 };
 
