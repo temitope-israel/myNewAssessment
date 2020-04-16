0865f194e9ef083d2c8516c0d86b58bf10e3c76c
temitope-israel <omoniyitemitopeisrael@gmail.com>
2020-04-16 05:03:07 +0100

    CCC

--- src/estimator.js
+++ src/estimator.js
@@ -1,19 +1,18 @@
-  let reportedCases;
-  let currentlyInfected;
-  let severeCurrentlyInfected;
-  let infectionsByRequestedTime;
-  let severeInfectionsByRequestedTime;
-  let periodType;
+let reportedCases;
+let currentlyInfected;
+let severeCurrentlyInfected;
+let infectionsByRequestedTime;
+let severeInfectionsByRequestedTime;
+let periodType; let input; let impact; let severeImpact;
 input = {
   reportedCases,
-  currentlyInfected,
-}
+  currentlyInfected
+  };
 
-impact = {}
-severeImpact = {}
+impact = {};
+severeImpact = {};
 const covid19ImpactEstimator = (data) => {
   data = input;
-   
   currentlyInfected = Math.trunc(input.reportedCases * 10);
   impact.currentlyInfected = currentlyInfected;
 
@@ -32,16 +31,16 @@ const covid19ImpactEstimator = (data) => {
 };
 };
 
-function estimatePeriods(days){
-days = 1024;
-let weeks;
-let months;
+function estimatePeriods(days) {
+  days = 1024;
+  let weeks;
+  let months;
 
-if (periodType === months) {
-  periodType = days / 30;
-} else if (periodType === weeks) {
-  periodType = days / 7;
-  infectionsByRequestedTime = Math.trunc(impact.currentlyInfected * periodType);
+  if (periodType === months) {
+      periodType = days / 30;
+  } else if (periodType === weeks) {
+      periodType = days / 7;
+      infectionsByRequestedTime = Math.trunc(impact.currentlyInfected * periodType);
   impact.infectionsByRequestedTime = infectionsByRequestedTime;
   severeInfectionsByRequestedTime = Math.trunc(severeImpact.severeCurrentlyInfected * periodType);
   severeImpact.severeInfectionsByRequestedTime = severeInfectionsByRequestedTime;
