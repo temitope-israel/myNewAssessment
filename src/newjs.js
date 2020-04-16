556907b24437c9865f984e7fefd06e79e42e5dbd
temitope-israel <omoniyitemitopeisrael@gmail.com>
2020-04-16 04:49:17 +0100

    CC

---
+++ src/newjs.js
@@ -0,0 +1,23 @@
+
+
+
+
+ //return data;
+ data = {
+  reportedCases: 40,
+  currentlyInfected: 6,
+  timespent: 50
+}
+
+function myName (here){
+  
+  here = here * 2;
+console.log(here);
+
+  
+ 
+}
+
+myName(data.currentlyInfected);
+myName(data.timespent);
+
