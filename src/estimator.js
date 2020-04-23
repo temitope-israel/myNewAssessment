c9a5a9b4d9f174abeafec4b29d4d2c94ee9dbeaa
temitope-israel <omoniyitemitopeisrael@gmail.com>
2020-04-23 11:48:29 +0100

    A2

--- src/estimator.js
+++ src/estimator.js
@@ -1,69 +1,68 @@
 let reportedCases;
 let currentlyInfected;
-let severeCurrentlyInfected;
+let severeCurrentlyInfected; 
+let myData; 
+let impact; 
+let severeImpact;
 const covid19ImpactEstimator = (data) => {
-myData = data;
+  myData = data;
 
-//  Input data
-myData = {
-periodType: "months",
-theDays: 50,
-reportedCases: 40
-};
+  //  Input data
+  myData = {
+    periodType: "days",
+    reportedCases: 40
+  };
 
-impact = {
-currentlyInfected: 20
-};
+  impact = {
+    currentlyInfected: 20
+  };
 
-severeImpact = {
-currentlyInfected: 40
-};
+  severeImpact = {
+    currentlyInfected: 40
+  };
 
-reportedCases = myData.reportedCases;
-currentlyInfected = reportedCases * 10;
-severeCurrentlyInfected = reportedCases * 50;
+  reportedCases = myData.reportedCases;
+  currentlyInfected = reportedCases * 10;
+  severeCurrentlyInfected = reportedCases * 50;
 
-//  Output Estimate of Currently Infected
-impact.currentlyInfected = currentlyInfected;
-severeImpact.currentlyInfected = severeCurrentlyInfected;
+  //  Output Estimate of Currently Infected
+  impact.currentlyInfected = currentlyInfected;
+  severeImpact.currentlyInfected = severeCurrentlyInfected;
 
-//  Call of INfections by requested time calculation function
-getPeriod(myData.periodType);
+  //  Call of INfections by requested time calculation function
+  getPeriod(myData.periodType);
 
-return {
-  data, 
-  impact, 
-  severeImpact
-}
-}//  Function Main Ends Here....
+  return {
+    data,
+    impact,
+    severeImpact
+  };
+};//  Function Main Ends Here....
 
 //  Calculation for Infections By requested Time
 function getPeriod(period) {
-  let days;
-  let weeks;
-  let months;
+  const days;
+  const weeks;
+  const months;
   months = 1024;
   days = Math.trunc(months / 30);
   weeks = Math.trunc(days * 7);
   months = Math.trunc(days * 30);
 
-  if (period === "days") {
+  if (period === 'days') {
     impact.infectionsByRequestedTime = impact.currentlyInfected * days;
     severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * days;
-  } 
-  else if (period === "weeks") {
+  } else if (period === 'weeks') {
     impact.infectionsByRequestedTime = impact.currentlyInfected * weeks;
-    severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * weeks;  
-  } 
-  else if (period === "months") {
+    severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * weeks;
+  } else if (period === 'months') {
     impact.infectionsByRequestedTime = impact.currentlyInfected * months;
     severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * months;
-  } 
-  else {
-    period = 0;
+  } else {
+    period === 0;
   }
-    
-  return {days, weeks, months}
-  }
+
+  return { days, weeks, months };
+}
 
 export default covid19ImpactEstimator;
