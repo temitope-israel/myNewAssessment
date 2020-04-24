ca60dc0ad4dd5b490a8dfe1fdf8c98216ffb3919
temitope-israel <omoniyitemitopeisrael@gmail.com>
2020-04-24 17:37:46 +0100

    Ctwelve2

--- src/estimator.js
+++ src/estimator.js
@@ -33,13 +33,12 @@ const covid19ImpactEstimator = (data) => {
   //  return { days, weeks, months };
   // Estimation in Weeks
 
-
     // For Projected Days
-    impact.infectionsByRequestedTime = impact.currentlyInfected
-    * Math.trunc(2 ** (infectionTime / 3));
+  impact.infectionsByRequestedTime = impact.currentlyInfected
+  * Math.trunc(2 ** (infectionTime / 3));
 
-    severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected
-    * Math.trunc(2 ** (infectionTime / 3));
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected
+  * Math.trunc(2 ** (infectionTime / 3));
 
   return {
     data: {
